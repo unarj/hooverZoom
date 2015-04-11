addeebb415f4e3fc4746d611a233ec62d370ab75
unarj <seth@on-rope.net>
2015-04-11 13:51:55 -0400

    removed the innerHTML call and stuff

--- data/panel.js
+++ data/panel.js
@@ -3,13 +3,28 @@ document.addEventListener('mousemove', hzMotion, true);
 document.addEventListener('wheel', function(event) { self.port.emit('wheel', event.deltaY) }, true);
 
 var i = document.getElementById('panelImg');
-var t = document.getElementById('panelText');
+var v = document.getElementById('panelVid');
 self.port.on('image', function(img, x, y, txt) {
+	v.style.display = 'none';
+	i.style.display = 'block';
 	i.src = img;
 	i.width = x;
 	i.height = y;
-	t.innerHTML = txt;
+	hzCaption(txt);
 });
+self.port.on('video', function(vid, x, y, txt) {
+	i.style.display = 'none';
+	v.style.display = 'block';
+    v.src = vid;
+	hzCaption(txt);
+});
+
+var t = document.getElementById('panelText');
+function hzCaption(str) {
+	str = str+" hooverZoom";
+	while(t.firstChild) { t.removeChild(t.firstChild) }
+	t.appendChild(document.createTextNode(str));
+}
 
 //this is a hack but works well enough for now...
 var hzMoved = 0;
