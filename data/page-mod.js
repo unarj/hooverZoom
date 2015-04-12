515b0f2c2ea4c8df1bbe8cfe1ed48a87c25f0bec
unarj <seth@on-rope.net>
2015-04-12 19:15:15 -0400

    moved all the load logic to worker
    decided it was clunky to have two different objects trying to load data
    into one panel, moving it all out of pageMod into pageWorker.  still
    needs some work.

--- data/page-mod.js
+++ data/page-mod.js
@@ -1,5 +1,4 @@
-//console.log("loading pageMod...");
-var hzCurAlb, hzCurImg, hzCurUrl, hzMark = false;
+var hzOnAlb = false;
 var hzLinks = document.getElementsByTagName('a');
 for (var i=0, l=hzLinks.length ; i < l; i++) {
 	hzLinks[i].addEventListener('mouseenter', hzMouseOn, false);
@@ -9,88 +8,14 @@ window.addEventListener('load', hzResize, false);
 window.addEventListener('resize', hzResize, false);
 window.addEventListener('wheel', hzWheel, false);
 
-var hzImage = new Image;
-hzImage.onload = function() {
-	if((hzCurImg === this.src) && this.width && this.height) {
-		var i = self.options.delay - (new Date().getTime() - hzMark);
-		if(i <= 0) {
-			self.port.emit('image', this.src, this.width, this.height);
-		} else {
-//			console.log("pageMod: image delayed "+i);
-			setTimeout( function(){ hzImage.src = hzCurImg }, i);
-		}
-	}
-}
-
-function hzMouseOn(event) {
-	target = event.target.toString();
-	if(hzCurUrl != target) {
-		hzMark = new Date().getTime();	
-		hzCurUrl = target;
-		var t = document.createElement('a');
-		t.href = event.target;
-		var p = t.hostname.split('.').reverse();
-		switch(p[1]+"."+p[0]) {
-			case "gfycat.com":
-				self.port.emit('worker', t.href);
-				return;
-			case "imgflip.com":
-				p = t.pathname.split('/');
-				if(p.length > 2) {
-					p = p[2].split('#');
-					t.href = t.protocol+"//i.imgflip.com/"+p[0]+".jpg";
-				}
-				break;
-			case "imgur.com":
-				p = t.pathname.split('/');
-				if((p[1] == "a") || (p[1] == "gallery")) {
-					hzCurAlb = t.protocol+"//imgur.com/a/"+p[2]+"?gallery"
-					self.port.emit('worker', hzCurAlb);
-					return;
-				} else {
-					t.href = t.protocol+"//i.imgur.com/"+p.pop();
-				}
-				p = t.pathname.split('.');
-				if(p.length == 1) { t.href += ".jpg"}
-				else switch(p.pop()) {
-					case "gif":
-						self.port.emit('worker', t.href+"v");
-						return;
-					case "gifv":
-						self.port.emit('worker', t.href);
-						return;
-				}
-				break;
-			case "livememe.com":
-				p = t.pathname.split('.');
-				if(p.length == 1) {	t.href = t.protocol+"//i.lvme.me"+p.pop()+".jpg" }
-				break;
-		}
-		hzImage.src = hzCurImg = t.href;
-//		if(hzCurUrl != hzCurImg) { console.log("pageMod: "+hzCurUrl+" -> "+hzCurImg) }
-	}
-}
-
-function hzMouseOff(event) {
-	if(!event || event.relatedTarget) {
-		hzImage.src = 'about:blank';
-		hzCurImg = hzCurUrl = false;
-		if(hzCurAlb) {
-			hzCurAlb = false;
-			self.port.emit('worker', 'about:blank');
-		}
-		self.port.emit('hide');
-	}
-}
-
+function hzMouseOn(event) { self.port.emit('worker', event.target.toString()) };
+function hzMouseOff(event) { if(!event || event.relatedTarget) { self.port.emit('worker') } }
 function hzResize(event) {
-	self.port.emit('winSize', window.innerWidth, window.innerHeight);
 	if(event.type == 'load') { window.removeEventListener('load', hzResize, false) }
-//	console.log(event.type);
+	self.port.emit('winSize', window.innerWidth, window.innerHeight);
 }
-
 function hzWheel(event) {
-	if(hzCurAlb) {
+	if(hzOnAlb) {
 		event.preventDefault();
 		event.stopPropagation();
 		self.port.emit('wheel', event.deltaY);
@@ -98,6 +23,5 @@ function hzWheel(event) {
 	}
 }
 
-//self.port.on('addHistory', function(url) { if(url) { window.open(url).close() } });
-self.port.on('click', function() { if(hzCurUrl) { window.location.href = hzCurUrl } });
-self.port.on('mouseOff', function() { hzMouseOff() });
+self.port.on('album', function(state) { hzOnAlbum = state });
+self.port.on('click', function(url) { if(url) { window.location.href = url } });
--- data/page-worker.js
+++ data/page-worker.js
@@ -1,18 +1,26 @@
-var hzImgs = [];
-var hzImgNum = 0;
-var hzImage = new Image;
-var hzMark = new Date().getTime();
-hzImage.onerror = function() { self.port.emit('hide') }
-hzImage.onload = function() {
-	var cur = this.src;
-	var i = self.options.delay - (new Date().getTime() - hzMark);
-	if(i <= 0) {
-		self.port.emit('image', this.src, this.width, this.height, (hzImgNum+1)+"/"+hzImgs.length);
-	} else {
-//		console.log("pageMod: image delayed "+i);
-		setTimeout( function(){ hzImage.src = cur }, i);
+var hzCurImg, hzCurUrl = false;
+var hzImg = new Image;
+	hzImg.onerror = function() { self.port.emit('hide') }
+	hzImg.onload = function() {
+		if(this.src == hzCurImg) {
+			var i = self.options.delay - (new Date().getTime() - hzMark);
+			if(i <= 0) {
+				var txt = this.src;
+				if(hzImgs.length > 0) { txt += " "+(hzImgNum+1)+"/"+hzImgs.length }
+				self.port.emit('image', this.src, this.width, this.height, txt);
+//				console.log("pageWorker showing: "+this.src);
+			} else {
+				var cur = this.src;
+				setTimeout( function(){ hzImg.src = cur }, i);
+//				console.log("pageWorker delaying: "+cur+" for: "+i);
+			}
+		} else {
+			console.log("pageWorker img blocked loading of: "+this.src);
+		}
 	}
-}
+var hzImgNum = 0;
+var hzImgs = [];
+var hzMark = new Date().getTime();
 
 var t = document.createElement('a');
 t.href = document.URL;
@@ -34,10 +42,63 @@ function hzLoadVideo() {
 		}
 	}
 	if(video && width && height) {
-		setTimeout( function(){ self.port.emit('video', video, width, height) }, self.options.delay);
+//		delays displaying video create wierdness since there's no "mouseout" call to clear the timer...
+		self.port.emit('video', video, width, height);
 	}
 //	console.log("pageWorker: loading "+video+" ("+width+"x"+height+")");
 }
+
+self.port.on('inspect', function(url) {
+	if(!url) { self.port.emit('load', 'about:blank') }
+	else if(url != hzCurUrl) {
+		hzMark = new Date().getTime();	
+		var t = document.createElement('a');
+		t.href = hzCurUrl = url;
+		var p = t.hostname.split('.').reverse();
+		switch(p[1]+"."+p[0]) {
+			case "gfycat.com":
+				win.URL = t.href;
+				t.href = '';
+				break;
+			case "imgflip.com":
+				p = t.pathname.split('/');
+				if(p.length > 2) {
+					p = p[2].split('#');
+					t.href = t.protocol+"//i.imgflip.com/"+p[0]+".jpg";
+				}
+				break;
+			case "imgur.com":
+				p = t.pathname.split('/');
+				if((p[1] == "a") || (p[1] == "gallery")) {
+					self.port.emit('load', t.protocol+"//imgur.com/a/"+p[2]+"?gallery");
+					t.href = null;
+					break;
+				} else {
+					t.href = t.protocol+"//i.imgur.com/"+p.pop();
+				}
+				p = t.pathname.split('.');
+				if(p.length == 1) { t.href += ".jpg"}
+				else switch(p.pop()) {
+					case "gif":
+						self.port.emit('load', t.href+"v");
+						t.href = null;
+						break;
+					case "gifv":
+						self.port.emit('load', t.href);
+						t.href = null;
+						break;
+				}
+				break;
+			case "livememe.com":
+				p = t.pathname.split('.');
+				if(p.length == 1) {	t.href = t.protocol+"//i.lvme.me"+p.pop()+".jpg" }
+				break;
+		}
+		if(t.href) { hzImg.src = hzCurImg = t.href }
+//		console.log(hzImg.src);
+//		document.removeChild(t);
+	}
+});
 
 self.port.on('wheel', function(delta) {
 	if(delta > 0) {
@@ -47,7 +108,7 @@ self.port.on('wheel', function(delta) {
 		hzImgNum--;
 		if(hzImgNum < 0) { hzImgNum = hzImgs.length-1 }
 	}
-	hzImage.src = hzImgs[hzImgNum];
+	hzImg.src = hzCurImg = hzImgs[hzImgNum];
 //	console.log("pageWorker sending: "+hzImgs[hzImgNum]+" ("+(hzImgNum+1)+" of "+hzImgs.length+")");
 });
 
@@ -71,7 +132,7 @@ switch(p[1]+"."+p[0]) {
 					delay += 500;
 				}
 			}
-			if(hzImgs.length > 0) {	hzImage.src = hzImgs[hzImgNum] }
+			if(hzImgs.length > 0) {	hzImg.src = hzCurImg = hzImgs[hzImgNum] }
 		}
 		break;
 }
