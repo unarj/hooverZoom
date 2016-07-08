ba9ec9beb81bd97b75c469d34f56fc421831df13
unarj <arjuna@on-rope.net>
2016-07-08 10:50:55 -0400

    reorg + small fixes

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
+function debug(str){ if(prefs.debug){ console.log(str) } }
 
 const { Cc, Ci } = require('chrome');
 const history = Cc['@mozilla.org/browser/history;1'].getService(Ci.mozIAsyncHistory);
 const uri = Cc['@mozilla.org/docshell/urifixup;1'].createInstance(Ci.nsIURIFixup).createFixupURI;
 
+var prefs = require('sdk/simple-prefs').prefs;
+prefs.enabled = true;
+prefs.debug = false;
+
 var curUrl = '';
 var pageMod = require('sdk/page-mod').PageMod;
 var pageWorker = require('sdk/page-worker').Page;
-var prefs = require('sdk/simple-prefs').prefs;
 var tabs = require('sdk/tabs');
-
-prefs.enabled = true;
-prefs.debug = false;
-function debug(str){ if(prefs.debug){ console.log(str) } }
 
 function addHist(url){
 	if(url){ history.updatePlaces({ uri:uri(url, 0), visits:[{transitionType:1, visitDate:Date.now()*1000}] }) }
@@ -19,6 +19,7 @@ function addHist(url){
 }
 
 function setPrefs(){
+	var loaded = true;
 	if(prefs.blacklist){
 		inc = ['*'];
 		exc = prefs.domainlist.split(',');
@@ -26,7 +27,7 @@ function setPrefs(){
 		inc = prefs.domainlist.split(',');
 		exc = [];
 	}
-	if(pageMod.destroy){ pageMod.destroy() }
+	if(pageMod.destroy){ pageMod.destroy() }else{ loaded = false }
 	pageMod = require('sdk/page-mod').PageMod({
 		include:inc, exclude:exc,
 		contentScriptFile:['./jquery-2.2.3.min.js', './page-mod.js'],
@@ -40,7 +41,7 @@ function setPrefs(){
 			w.port.on('visit', addHist);
 		}
 	});
-	if(pageWorker.destroy){ pageWorker.destroy() }
+	if(pageWorker.destroy){ pageWorker.destroy() }else{ loaded = false }
 	pageWorker = require('sdk/page-worker').Page({
 		contentScriptFile:'./page-worker.js',
 		contentScriptOptions:{ 'prefs':prefs },
@@ -48,7 +49,7 @@ function setPrefs(){
 	});
 	pageWorker.port.on('done', function(){ pageWorker.contentURL = 'about:blank' });
 	pageWorker.port.on('found', function(imgs){ pageMod.port.emit('found', curUrl, imgs); });
-	for(let tab of tabs){ tab.reload() }
+	if(loaded){ for(let tab of tabs){ if(tab.url != 'about:addons'){ tab.reload() } } }
 }
 
 function toggle(){
